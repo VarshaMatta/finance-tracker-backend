2c8ad4c61d4dfcb195df70a7541d3ecfba107765
VarshaMatta <varshamatta3@gmail.com>
2025-04-06 19:36:21 +0530

    fix: use environment variable for MongoDB connection.

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,8 @@ const app: Express = express();
 const port = process.env.PORT || 3001;
 
 // Use only one MongoDB connection - the local one
-mongoose
-  .connect("mongodb://localhost:27017/finance-tracker")
+mongoose.connect(process.env.MONGO_URI)
+
   .then(() => {
     console.log("Connected to local MongoDB");
   })
